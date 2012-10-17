fadcf2fe32f72c6cb3a9a5a71ab560d156803df6
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2012-10-16 22:52:09 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyFinder::Application.config.secret_token = '22f3480b463d426060728b00746061f51843e80a10824bade61d8690a01389be061361d35ebb9baccfdbdbb1536f5b6cb6b2b4af6f5f42b5f3e144e842866526'
