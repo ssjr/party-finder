3f762094245e0baec487569761a68d33a0f1f962
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2012-10-16 23:46:12 -0300

    Validates presence of name and description on Party

--- app/models/party.rb
+++ app/models/party.rb
@@ -2,4 +2,6 @@ class Party < ActiveRecord::Base
   belongs_to :city
   belongs_to :state
   attr_accessible :description, :latitude, :longitude, :name, :start_at, :street, :street_extra, :street_number
+
+  validates_presence_of :name, :description, :start_at
 end
