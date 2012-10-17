33062ed45989e8d277b930de46ed35144c1be632
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2012-10-16 23:55:55 -0300

    Party form (references)

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,7 +1,7 @@
 class Party < ActiveRecord::Base
   belongs_to :city
   belongs_to :state
-  attr_accessible :description, :latitude, :longitude, :name, :start_at, :street, :street_extra, :street_number
+  attr_accessible :description, :latitude, :longitude, :name, :start_at, :street, :street_extra, :street_number, :city_id, :state_id
 
   validates_presence_of :name, :description, :start_at
 end
